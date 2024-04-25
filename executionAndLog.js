689f577c9b098c3ee0fb1577de6d4fc1f2d9cff5
Serhii Myshko <sergeiown@gmail.com>
2024-04-25 20:53:14 +0300

    develop a modular structure

---
+++ executionAndLog.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const os = require('os');
+const { exec } = require('child_process');
+
+function executeAndLog(command, logFilePath, callback) {
+    console.clear();
+
+    const currentDate = new Date().toLocaleString();
+
+    fs.appendFileSync(logFilePath, `${os.EOL}>> ${currentDate}${os.EOL}`);
+
+    const childProcess = exec(command);
+
+    const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+
+    childProcess.stdout.on('data', (data) => {
+        const lines = data.toString().split(os.EOL);
+
+        lines.forEach((line) => {
+            const trimmedLine = line.trim();
+
+            if (!/[░▒█]/.test(trimmedLine) && /[a-zA-Zа-яА-Я0-9]/.test(trimmedLine)) {
+                logStream.write(trimmedLine + os.EOL);
+            }
+        });
+    });
+
+    childProcess.stderr.pipe(logStream);
+
+    childProcess.stdout.pipe(process.stdout);
+    childProcess.stderr.pipe(process.stderr);
+
+    childProcess.on('exit', () => {
+        callback(logFilePath);
+    });
+}
+
+function checkAndTrimLogFile(logFilePath, maxFileSizeInBytes) {
+    const fileSizeInBytes = fs.statSync(logFilePath).size;
+
+    if (fileSizeInBytes > maxFileSizeInBytes) {
+        const logContent = fs.readFileSync(logFilePath, 'utf-8');
+        const blocks = logContent.split(`${os.EOL}${os.EOL}`);
+
+        if (blocks.length > 1) {
+            const trimmedLog = blocks.slice(1).join(`${os.EOL}${os.EOL}`);
+            fs.writeFileSync(logFilePath, trimmedLog, 'utf-8');
+            console.log(`Log file size reduced`);
+        } else {
+            fs.truncateSync(logFilePath, 0);
+        }
+    }
+}
+
+module.exports = {
+    executeAndLog,
+    checkAndTrimLogFile,
+};
---
+++ wingetCheck.js
@@ -0,0 +1,23 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+const os = require('os');
+
+function checkForWinget(logFilePath) {
+    const wingetCheckCommand = 'winget --version';
+
+    exec(wingetCheckCommand, (error) => {
+        if (error) {
+            console.error('Error: winget is not installed on this system.');
+
+            fs.appendFileSync(logFilePath, `${os.EOL}>> ${new Date().toLocaleString()}${os.EOL}`);
+            fs.appendFileSync(logFilePath, 'Error: winget is not installed on this system.');
+            process.exit(1);
+        } else {
+            console.log('Winget is installed on the system.');
+        }
+    });
+}
+
+module.exports = {
+    checkForWinget,
+};

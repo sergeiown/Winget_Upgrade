f125671eb612534007563e8c186e3baf110f2bb5
Serhii Myshko <sergeiown@gmail.com>
2024-04-26 12:45:49 +0300

    change and simplify the modular structure

---
+++ main.js
@@ -0,0 +1,44 @@
+/* Copyright (c) 2024 Serhii I. Myshko
+https://github.com/sergeiown/Winget_Upgrade/blob/main/LICENSE */
+
+'use strict';
+
+const fs = require('fs');
+const os = require('os');
+const { exec } = require('child_process');
+const { checkAndTrimLogFile, executeAndLog } = require('./utils');
+const settings = require('./settings');
+
+function checkForWinget() {
+    exec(settings.wingetPath, (error, stdout) => {
+        if (error) {
+            console.error('Error: winget is not installed on this system.');
+
+            fs.appendFileSync(settings.logFilePath, `${os.EOL}>> ${new Date().toLocaleString()}${os.EOL}`);
+            fs.appendFileSync(settings.logFilePath, 'Error: winget is not installed on this system.');
+            process.exit(1);
+        } else {
+            console.log(`Winget is installed on the system.${os.EOL}`);
+            const wingetLocation = stdout.trim();
+
+            const command = `${wingetLocation} ${settings.wingetArgs.join(' ')}`;
+
+            executeAndLog(command, settings.logFilePath, () => {
+                checkAndTrimLogFile(settings.logFilePath, settings.maxLogFileSize);
+                setTimeout(() => {
+                    process.stdin.setRawMode(true);
+                    process.stdin.resume();
+                    process.stdin.on('data', process.exit.bind(process, 0));
+
+                    console.log(settings.finalMessage);
+                }, 1000);
+
+                setTimeout(() => {
+                    process.exit(0);
+                }, 10000);
+            });
+        }
+    });
+}
+
+checkForWinget();
--- settings.js
+++ settings.js
@@ -5,21 +5,23 @@ https://github.com/sergeiown/Winget_Upgrade/blob/main/LICENSE */
 
 const os = require('os');
 const path = require('path');
-
 const documentsPath = path.join(os.homedir(), 'Documents');
-const logFilePath = path.join(documentsPath, 'winget_upgrade.log');
-const maxLogFileSize = 256 * 1024;
 
-const command =
-    'cmd /c winget upgrade --all --accept-package-agreements --accept-source-agreements --disable-interactivity --silent';
-
-const finalMessage = `${os.EOL}Update is complete.${os.EOL.repeat(
-    2
-)}Program will automatically exit after 10 seconds, or press any key to exit immediately.`;
-
-module.exports = {
-    logFilePath,
-    maxLogFileSize,
-    command,
-    finalMessage,
+const settings = {
+    wingetPath: 'where.exe winget',
+    logFilePath: path.join(documentsPath, 'winget_upgrade.log'),
+    maxLogFileSize: 256 * 1024,
+    wingetArgs: [
+        'upgrade',
+        '--all',
+        '--accept-package-agreements',
+        '--accept-source-agreements',
+        '--disable-interactivity',
+        '--silent',
+    ],
+    finalMessage: `${os.EOL}Update is complete.${os.EOL.repeat(
+        2
+    )}Program will automatically exit after 10 seconds, or press any key to exit immediately.`,
 };
+
+module.exports = settings;

904c0d46705fa58ee64217bfe339fb1d0fc44e3e
sergeiown <sergeiown@gmail.com>
2024-11-01 15:44:58 +0200

    Add a block for checking the existence of targetPath with the if not exist condition

--- start_menu_shortcut_enabler.bat
+++ start_menu_shortcut_enabler.bat
@@ -10,8 +10,6 @@ set folderPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 set shortcutPath=%folderPath%\%shortcutName%.lnk
 set iconPath=%SystemRoot%\System32\SHELL32.dll,13
 set workingDirectory=%CD%
-set windowStyle=1
-
 :: WindowStyle=
 :: 1 - Normal window: opens the window in a normal size and position
 :: 2 - Hidden window: window is hidden, process runs in the background
@@ -20,6 +18,13 @@ set windowStyle=1
 :: 5 - Minimized window: opens the window minimized (in taskbar)
 :: 6 - Minimized window with active focus: window is minimized, but still active
 :: 7 - Minimized window (same as 5): opens the window minimized
+set windowStyle=1
+
+if not exist "%targetPath%" (
+    echo Error: The target file "%targetPath%" does not exist. Operation aborted. & echo.
+    pause
+    exit /b
+)
 
 if exist "%shortcutPath%" (
     echo The shortcut "%shortcutPath%" exists. & echo. & echo Deleting the shortcut. & echo.
@@ -30,6 +35,3 @@ if exist "%shortcutPath%" (
 )
 
 echo The operation is complete. & echo. & pause
-
-
-

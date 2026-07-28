c8d84c6140b14d75867654770362463b4e96f150
sergeiown <sergeiown@gmail.com>
2026-07-28 22:57:38 +0300

    Bump to 3.0.0

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const os = require('os');
 const path = require('path');
 
 const settings = {
-    appVersion: '2.1.6',
+    appVersion: '3.0.0',
     wingetPath: 'where.exe winget',
     wingetVersion: 'winget --version',
     logFilePath: path.join(process.cwd(), 'winget_upgrade.log'),
